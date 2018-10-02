ab8f5b29ea204e534ff902f751d6d299cfb13028
kishor tare <tarekishor@gmail.com>
2018-10-01 23:11:21 -0400

    Added HTML templates for rendering newsfeed #7

--- empower/src/main/java/com/soen/empower/controller/TeacherController.java
+++ empower/src/main/java/com/soen/empower/controller/TeacherController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.soen.empower.entity.Card;
@@ -33,7 +34,7 @@ public class TeacherController {
 		return model;
 	}
 
-	@RequestMapping("/addPost")
+	@RequestMapping(value="/addPost", method=RequestMethod.POST)
 	public String addPost(@ModelAttribute Card card) {
 		cardService.add(card);
 		return "redirect:/teacher/home";

842cf9b0c1eae1622b838e8d637014ff43957e10
varun <varunsinghal15@gmail.com>
2018-10-12 12:39:25 -0400

    feeds to be displayed in reverse order on homepage #38

--- empower/src/main/java/com/soen/empower/repository/CardRepository.java
+++ empower/src/main/java/com/soen/empower/repository/CardRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.soen.empower.entity.Card;
 
+import java.util.List;
+
 public interface CardRepository extends CrudRepository<Card, String>{
+    public List<Card> findAllByOrderByIdDesc();
 
 }

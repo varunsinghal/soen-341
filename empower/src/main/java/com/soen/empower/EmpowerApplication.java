a069eb5e4fa416466a7791e82b8a3de690d1a553
varunsinghal <varunsinghal15@gmail.com>
2018-10-21 12:51:38 -0400

    Added java doc for the main application listing down the key features used in the spring application for the soen 341, the empower application #73

--- empower/src/main/java/com/soen/empower/EmpowerApplication.java
+++ empower/src/main/java/com/soen/empower/EmpowerApplication.java
@@ -4,17 +4,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * The Class EmpowerApplication.
+ * Empower is Spring Boot application. This makes it easy to create stand-alone production-grade
+ * spring based applications. Key features:
+ * <ul>
+ * <li>Embedded Tomcat</li>
+ * <li>No need to deploy WAR files</li>
+ * <li>No XML configuration</li>
+ * <li>Health checks using actuator and dev tools</li>
+ * <li>Automatic dependency managment of Spring and 3rd party libraries.</li>
+ * </ul>
+ * <p>
+ * The java doc comments are referenced using the guide present in the link below.
+ * <a href="https://www.oracle.com/technetwork/java/javase/documentation/index-137868.html#format">CLick here.</a>
+ * </p>
+ *
+ * @author Varun Singhal
+ * @since 1.0
  */
 @SpringBootApplication
 public class EmpowerApplication {
 
+
 	/**
-	 * The main method.
+	 * The method which launches the application with system arguments provided by the console.
 	 *
-	 * @param args the arguments
+	 * @param args system arguments, if required to launch the console application
 	 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		SpringApplication.run(EmpowerApplication.class, args);
 	}
 }

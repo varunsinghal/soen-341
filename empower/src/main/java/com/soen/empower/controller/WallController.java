92e927a36662ebf956623d219ee37cc6d97f4ccb
varunsinghal <varunsinghal15@gmail.com>
2018-10-27 12:13:14 -0400

    Added friend based access to the wall #109

--- empower/src/main/java/com/soen/empower/controller/WallController.java
+++ empower/src/main/java/com/soen/empower/controller/WallController.java
@@ -1,9 +1,12 @@
 package com.soen.empower.controller;
 
 import com.soen.empower.service.CardService;
+import com.soen.empower.service.FriendService;
 import com.soen.empower.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,15 +18,32 @@ public class WallController {
 
     @Autowired
     private CardService cardService;
+
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private FriendService friendService;
+
     @RequestMapping("")
-    public ModelAndView indexWall(HttpSession session){
+    public ModelAndView indexWall(HttpSession session) {
         ModelAndView model = new ModelAndView("wall/index");
         long userId = (long) session.getAttribute("user_id");
         model.addObject("cards", cardService.fetchCardsFor(userId));
         model.addObject("owner", userService.findById(userId));
         return model;
     }
+
+    @RequestMapping("/{id}")
+    public ModelAndView indexWall(HttpSession session, @PathVariable("id") long userId) {
+        if (friendService.areFriends((long) session.getAttribute("user_id"), userId).equals("1")) {
+            ModelAndView model = new ModelAndView("wall/index");
+            model.addObject("cards", cardService.fetchCardsFor(userId));
+            model.addObject("owner", userService.findById(userId));
+            return model;
+        }
+        throw new AccessDeniedException("403 returned");
+    }
+
+
 }

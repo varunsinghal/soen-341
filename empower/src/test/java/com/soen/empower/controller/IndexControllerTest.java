2e34cb7907e59588174394ce374c07afde16c7c4
admiralponce <dorynakad@gmail.com>
2018-10-28 12:19:44 -0400

    Added mockito bean for userService #132

--- empower/src/test/java/com/soen/empower/controller/IndexControllerTest.java
+++ empower/src/test/java/com/soen/empower/controller/IndexControllerTest.java
@@ -1,9 +1,11 @@
 package com.soen.empower.controller;
 
+import com.soen.empower.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,6 +17,9 @@ public class IndexControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
 
     @Test
     public void getIndex_ReturnsLoginPage() throws Exception {

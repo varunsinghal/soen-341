8f5b09967ed2ce359b08a719f04faa1ba867c31d
varunsinghal <varunsinghal15@gmail.com>
2018-10-29 21:34:05 -0400

    Added create form to add new groups #112

--- empower/src/main/java/com/soen/empower/controller/GroupController.java
+++ empower/src/main/java/com/soen/empower/controller/GroupController.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/group")
@@ -17,9 +20,24 @@ public class GroupController {
     private GroupService groupService;
 
     @RequestMapping("")
-    public ModelAndView indexGroup() {
+    public ModelAndView indexGroup(HttpSession session) {
         ModelAndView model = new ModelAndView("group/index");
         model.addObject("groups", groupService.fetchAll());
+        return model;
+    }
+
+    @RequestMapping("/all")
+    public ModelAndView allGroup() {
+        ModelAndView model = new ModelAndView("group/index");
+        model.addObject("groups", groupService.fetchAll());
+        return model;
+    }
+
+    @RequestMapping("/search")
+    public ModelAndView searchGroup(@RequestParam("name") String search){
+        ModelAndView model = new ModelAndView("group/index");
+        model.addObject("groups", groupService.findByPartialName(search));
+        model.addObject("searchString", search);
         return model;
     }
 
--- empower/src/main/java/com/soen/empower/repository/GroupRepository.java
+++ empower/src/main/java/com/soen/empower/repository/GroupRepository.java
@@ -3,5 +3,8 @@ package com.soen.empower.repository;
 import com.soen.empower.entity.Group;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GroupRepository extends CrudRepository<Group, Long> {
+    List<Group> findByNameContainingIgnoreCase(String name);
 }

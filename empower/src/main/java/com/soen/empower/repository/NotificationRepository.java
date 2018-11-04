2b0495b4ea50ec22c451a7d53b39f1bc53d8047d
varunsinghal <varunsinghal15@gmail.com>
2018-11-03 22:47:17 -0400

    Added historical notification list in the session #169

--- empower/src/main/java/com/soen/empower/repository/NotificationRepository.java
+++ empower/src/main/java/com/soen/empower/repository/NotificationRepository.java
@@ -3,5 +3,8 @@ package com.soen.empower.repository;
 import com.soen.empower.entity.Notification;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface NotificationRepository extends CrudRepository<Notification, Long> {
+    List<Notification> findByUserId(long id);
 }
